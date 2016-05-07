329bf5dcf6abdd42f905b96cf87999346f78bc6d
root <root@ip-172-31-39-114.us-west-2.compute.internal>
2016-05-07 01:01:28 -0400

    init

--- project/Build.scala
+++ project/Build.scala
@@ -6,13 +6,15 @@ object ApplicationBuild extends Build {
 
   val appName         = "starter-java"
   val appVersion      = "1.0-SNAPSHOT"
+  val scalaVersion    = "2.9.2"
+
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
     javaEbean,
-    "com.twilio.sdk" % "twilio-java-sdk" % "5.2.1"
+    "com.twilio.sdk" % "twilio-java-sdk" % "5.10.0"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
